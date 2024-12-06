ce071b0350577a4c83ab20657fc794af72eb049f
graydinosaur <notahacker256@outlook.com>
2024-12-06 15:00:51 +0100

    added registration form

--- website/base/urls.py
+++ website/base/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
-
-urlpatterns =  [
+from .views import AuthView, home
+urlpatterns =  [    
+    path("", home, name="home"),
+    path("signup/", AuthView, name="authView"),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
--- website/base/views.py
+++ website/base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required
+def home(request):
+	return render(request, "home.html", {})
+
+def AuthView(request):
+	form = UserCreationForm()
+	return render(request,"registration/signup.html",{"form":form})
